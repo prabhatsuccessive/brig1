655cc4dca7164a9b794851a1c3359fe0c7244fda
Prabhat Ranjan <prabhat.ranjan@prabhatr.successivetech.com>
2020-10-09 14:13:58 +0530

    add dockkerfile

--- brigade.js
+++ brigade.js
@@ -1,9 +1,75 @@
 const { events, Job } = require("brigadier");
-events.on("exec", () => {
-  var hello = new Job("hello", "alpine:3.4") 
-hello.tasks = [ 
-"echo Hello",
-"echo World", 
+
+ events.on("exec", function(e, project) {
+  console.log("received push for commit " + e.revision.commit)
+
+  // Create a new job
+ 
+   var docker = new Job("docker" , "docker:dind");
+   
+    docker.privileged = true;
+    docker.env = {
+    DOCKER_DRIVER: "overlay"
+    };
+  docker.env.DOCKER_USER = project.secrets.dockerLogin
+  docker.env.DOCKER_PASS = project.secrets.dockerPass
+
+  docker.tasks = [
+    "dockerd-entrypoint.sh &",
+    "sleep 10",
+    "cd src",
+    "pwd",
+    "cat README.md",
+    "ls -lart",
+    "docker build -t prabhat2346/brigade:12 .",
+    "docker login docker.io -u $DOCKER_USER -p $DOCKER_PASS",
+    "docker push prabhat2346/brigade:12",
+    "docker images"
 ]
-  job.run();
-});
+
+  docker.streamLogs = true;
+
+  var hello = new Job("hello", "alpine:3.4")
+  hello.tasks = [
+    "echo Hello",
+    "echo World",
+  ]
+
+ var goodbye = new Job("goodbye", "alpine:3.4")
+  goodbye.tasks = [
+    "echo Goodbye",
+    "echo World",
+ ]
+
+ // We're done configuring, so we run the job
+ // hello.run()
+ // goodbye.run()
+//  docker.run()
+//events.on("exec", (e, project) => {
+ //let pipeline = new pipeline()
+console.log(e)
+console.log(e.revision.ref)
+if (e.revision.ref == "master")
+
+{
+
+docker.run()
+
+}
+
+else if (e.revision.ref == "develop")
+
+{
+ 
+ goodbye.run()
+}
+
+else (e.revision.ref == "fb1")
+
+{
+
+hello.run()
+
+}
+
+})
